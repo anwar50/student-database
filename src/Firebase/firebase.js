3ae1f9a134e34edb36213b98ce68ae68c0ab7d37
Anwar Abdi <anwar.abdi@sky.uk>
2019-08-15 21:33:08 +0100

    adding config tokens

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -4,7 +4,13 @@ import firestore from "firebase/firestore"
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-
+    apiKey: "AIzaSyAperwxzQjKh4MpjWCsBJSEvVAGlAGmywM",
+    authDomain: "my-amazing-project-14de6.firebaseapp.com",
+    databaseURL: "https://my-amazing-project-14de6.firebaseio.com",
+    projectId: "my-amazing-project-14de6",
+    storageBucket: "my-amazing-project-14de6.appspot.com",
+    messagingSenderId: "176236111087",
+    // appId: "1:176236111087:web:1f27fa497f57e1b1"
 }
     //initializing app
 firebase.initializeApp(config)
